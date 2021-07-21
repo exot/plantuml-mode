exot/plantuml-mode

commit 051d17efba7dbdcf1eb462131b69d8639263c744

Fix regexp errors from automatic conversion of \s to [[:blank:]]

plantuml-mode.el
@@ -582,7 +582,7 @@ or it is followed by line end.")
 (defvar plantuml-indent-regexp-newif-start "^[[:blank:]]*\\(?:else\\)?if[[:blank:]]+(.*)[[:blank:]]+then[[:blank:]]*.*$")
 (defvar plantuml-indent-regexp-loop-start "^[[:blank:]]*\\(?:repeat[[:blank:]]*\\|while[[:blank:]]+(.*).*\\)$")
 (defvar plantuml-indent-regexp-fork-start "^[[:blank:]]*\\(?:fork\\|split\\)\\(?:[[:blank:]]+again\\)?[[:blank:]]*$")
-(defvar plantuml-indent-regexp-case-start "^[[:blank:]]*\\(?:switch\\|case\\)[[:blank:]]-*(.*)[[:blank:]]*$")
+(defvar plantuml-indent-regexp-case-start "^[[:blank:]]*\\(?:switch\\|case[[:blank:]]*(.*)\\)[[:blank:]]*$")
 (defvar plantuml-indent-regexp-macro-start "^[[:blank:]]*!definelong.*$")
 (defvar plantuml-indent-regexp-user-control-start "^.*'.*[[:blank:]]*PLANTUML_MODE_INDENT_INCREASE[[:blank:]]*.*$")
 (defvar plantuml-indent-regexp-start (list plantuml-indent-regexp-block-start
@@ -616,7 +616,7 @@ or it is followed by line end.")
 (defvar plantuml-indent-regexp-newif-end "^[[:blank:]]*\\(endif\\|elseif\\|else\\)[[:blank:]]*.*$")
 (defvar plantuml-indent-regexp-loop-end "^[[:blank:]]*\\(repeat[[:blank:]]*while\\|endwhile\\)[[:blank:]]*.*$")
 (defvar plantuml-indent-regexp-fork-end "^[[:blank:]]*\\(\\(fork\\|split\\)[[:blank:]]+again\\|end[[:blank:]]+\\(fork\\|split\\)\\)[[:blank:]]*$")
-(defvar plantuml-indent-regexp-case-end "^[[:blank:]]*\\(case[[:blank:]]-*([^)]*)\\|endswitch\\)[[:blank:]]*\\('.*\\)?$")
+(defvar plantuml-indent-regexp-case-end "^[[:blank:]]*\\(case[[:blank:]]*([^)]*)\\|endswitch\\)[[:blank:]]*\\('.*\\)?$")
 (defvar plantuml-indent-regexp-macro-end "^[[:blank:]]*!enddefinelong[[:blank:]]*\\('.*\\)?$")
 (defvar plantuml-indent-regexp-user-control-end "^.*'.*[[:blank:]]*PLANTUML_MODE_INDENT_DECREASE[[:blank:]]*.*$")
 (defvar plantuml-indent-regexp-end (list plantuml-indent-regexp-block-end
@@ -647,14 +647,14 @@ provide a non-nil value for FORCE."
     (when (or (not plantuml-kwdList)
               force)
       (plantuml-init mode)
-      (let ((plantuml-types-regexp (concat "^[[:blank:]] *\\("
+      (let ((plantuml-types-regexp (concat "^[[:blank:]]*\\("
                                            (regexp-opt plantuml-types 'words)
-                                           "\\|\\<\\(note[[:blank:]] +over\\|note[[:blank:]] +\\(left\\|right\\|bottom\\|top\\)[[:blank:]] +\\(of\\)?\\)\\>\\|\\<\\(\\(left\\|center\\|right\\)[[:blank:]] +\\(header\\|footer\\)\\)\\>\\)"))
-            (plantuml-keywords-regexp (concat "^[[:blank:]] *"
+                                           "\\|\\<\\(note[[:blank:]]+over\\|note[[:blank:]]+\\(left\\|right\\|bottom\\|top\\)[[:blank:]]+\\(of\\)?\\)\\>\\|\\<\\(\\(left\\|center\\|right\\)[[:blank:]]+\\(header\\|footer\\)\\)\\>\\)"))
+            (plantuml-keywords-regexp (concat "^[[:blank:]]*"
                                               (regexp-opt plantuml-keywords 'words)
                                               "\\|\\(<\\|<|\\|\\*\\|o\\)\\(\\.+\\|-+\\)\\|\\(\\.+\\|-+\\)\\(>\\||>\\|\\*\\|o\\)\\|\\.\\{2,\\}\\|-\\{2,\\}"))
             (plantuml-builtins-regexp (regexp-opt plantuml-builtins 'words))
-            (plantuml-preprocessors-regexp (concat "^[[:blank:]] *"
+            (plantuml-preprocessors-regexp (concat "^[[:blank:]]*"
                                                    (regexp-opt plantuml-preprocessors 'words))))
 
         (setq plantuml-font-lock-keywords

commit e9ad06c3d957d790285e952180a5e49d75934997

Fix erroneously changed indent regexp for detecting switch statement
Both the switch and the case keyword take an argument with parenthese, but the
previous regexp only included the parentheses of the case statement.  This error
had been introduced in 051d17e while fixing wrong regular expressions coming
from the transition from \s to [[:blank:]].

plantuml-mode.el
@@ -582,7 +582,7 @@ or it is followed by line end.")
 (defvar plantuml-indent-regexp-newif-start "^[[:blank:]]*\\(?:else\\)?if[[:blank:]]+(.*)[[:blank:]]+then[[:blank:]]*.*$")
 (defvar plantuml-indent-regexp-loop-start "^[[:blank:]]*\\(?:repeat[[:blank:]]*\\|while[[:blank:]]+(.*).*\\)$")
 (defvar plantuml-indent-regexp-fork-start "^[[:blank:]]*\\(?:fork\\|split\\)\\(?:[[:blank:]]+again\\)?[[:blank:]]*$")
-(defvar plantuml-indent-regexp-case-start "^[[:blank:]]*\\(?:switch\\|case[[:blank:]]*(.*)\\)[[:blank:]]*$")
+(defvar plantuml-indent-regexp-case-start "^[[:blank:]]*\\(?:switch\\|case\\)[[:blank:]]*(.*)[[:blank:]]*$")
 (defvar plantuml-indent-regexp-macro-start "^[[:blank:]]*!definelong.*$")
 (defvar plantuml-indent-regexp-user-control-start "^.*'.*[[:blank:]]*PLANTUML_MODE_INDENT_INCREASE[[:blank:]]*.*$")
 (defvar plantuml-indent-regexp-start (list plantuml-indent-regexp-block-start